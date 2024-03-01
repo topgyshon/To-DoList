e9c7bf35980a332fe25f61f884288ff1166b6ba9
topgyshon <150448310+topgyshon@users.noreply.github.com>
2024-03-01 14:56:15 +0100

    EDITED

--- build/index.js
+++ build/index.js
@@ -5,6 +5,21 @@ const taskList = document.getElementById("taskList");
 const inputSection = document.getElementById("taskInputSection");
 let mydoList = [];
 sub.addEventListener("click", () => {
-    mydoList.push(input.value);
-    taskList.innerHTML = `<li> ${input.value}</li>`;
+    if (input.value.length > 0) {
+        mydoList.push(input.value);
+        input.value = '';
+        displayTask();
+    }
 });
+const displayTask = () => {
+    let listHTML = ``;
+    for (let i = 0; i < mydoList.length; i++) {
+        listHTML += `<li> ${mydoList[1]} <br> <button> Edit</button> <button onclick= 'deleteTask(${i})''> Completed</buttton> </li>`;
+    }
+    ;
+    taskList.innerHTML = listHTML;
+};
+const deleteTask = (index) => {
+    mydoList.splice(index, 1);
+    displayTask();
+};
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,24 @@
 let mydoList:string[] = [];
 
 sub.addEventListener("click", ()=>{
+  if (input.value.length > 0){
     mydoList.push(input.value);
-    taskList.innerHTML = `<li> ${input.value}</li>`;
-})
+    input.value = '';
+    displayTask();
+  }
+});
+
+const displayTask = () => {
+  let listHTML: string = ``
+
+  for (let i:number = 0; i < mydoList   .length; i++) {
+   listHTML += `<li> ${mydoList [1]} <br> <button> Edit</button> <button onclick= 'deleteTask(${i})''> Completed</buttton> </li>`;
+  };
+  taskList.innerHTML = listHTML
+}
+
+const deleteTask = (index: number) => {
+  mydoList.splice(index, 1); 
+  displayTask();
+}
 
